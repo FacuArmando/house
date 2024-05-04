532c9782b5059bd4ea239011c805f42170617656
Facu Armando <facundoarmando57@gmail.com>
2024-05-04 20:18:40 -0300

    Intercambio inicial de proyecto

---
+++ Picture.java
@@ -0,0 +1,109 @@
+/**
+ * This class represents a simple picture. You can draw the picture using
+ * the draw method. But wait, there's more: being an electronic picture, it
+ * can be changed. You can set it to black-and-white display and back to
+ * colors (only after it's been drawn, of course).
+ *
+ * This class was written as an early example for teaching Java with BlueJ.
+ * 
+ * @author  Michael Kšlling and David J. Barnes
+ * @version 2016.02.29
+ */
+public class Picture
+{
+    private Square wall;
+    private Square window;
+    private Triangle roof;
+    private Circle sun;
+    private boolean drawn;
+    private Person persona;
+
+    /**
+     * Constructor for objects of class Picture
+     */
+    public Picture()
+    {
+        wall = new Square();
+        window = new Square();
+        roof = new Triangle();  
+        sun = new Circle();
+        persona = new Person();
+        drawn = false;
+    }
+
+    /**
+     * Draw this picture.
+     */
+    public void draw()
+    {
+        if(!drawn) {
+            wall.moveHorizontal(-140);
+            wall.moveVertical(20);
+            wall.changeSize(120);
+            wall.makeVisible();
+            
+            window.changeColor("black");
+            window.moveHorizontal(-120);
+            window.moveVertical(40);
+            window.changeSize(40);
+            window.makeVisible();
+    
+            roof.changeSize(60, 180);
+            roof.moveHorizontal(20);
+            roof.moveVertical(-60);
+            roof.makeVisible();
+    
+            sun.changeColor("yellow");
+            sun.moveHorizontal(100);
+            sun.moveVertical(-40);
+            sun.slowMoveVertical(350);
+            sun.changeSize(80);
+            sun.makeVisible();
+            
+            /*Ejercicio 8 (Practica 2):
+             sun.slowMoveHorizontal(-410);
+             sun.makeInvisible();
+             sun.moveHorizontal(610);
+             sun.moveVertical(0);
+             sun.makeVisible();
+             sun.slowMoveHorizontal(-200);*/ 
+             
+             drawn = true;
+        }
+    }
+
+    /**
+     * Change this picture to black/white display
+     */
+    public void setBlackAndWhite()
+    {
+        wall.changeColor("black");
+        window.changeColor("white");
+        roof.changeColor("black");
+        sun.changeColor("black");
+    }
+
+    /**
+     * Change this picture to use color display
+     */
+    public void setColor()
+    {
+        wall.changeColor("red");
+        window.changeColor("black");
+        roof.changeColor("green");
+        sun.changeColor("yellow");
+    }
+    
+    /**
+     * Ejercicio 1.19: Puesta del sol, en un metodo separado
+     */
+    public void sunset(){
+       sun.slowMoveVertical(350);
+       
+       //Ejercicio 1.20
+       persona.slowMoveHorizontal(110);
+       persona.makeInvisible();
+    }
+    
+    
+}
